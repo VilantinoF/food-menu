d2bdfcde0bb8e2b6c3dd1617c7f72b6a72584e3f
Vilantino Fernandion <vilantinof@pop-os.localdomain>
2021-10-23 23:37:36 +0700

    mengumpulkan tugas menu makanan

--- app/src/main/java/com/example/aplikasimenumakanan/MainActivity.java
+++ app/src/main/java/com/example/aplikasimenumakanan/MainActivity.java
@@ -31,28 +31,21 @@ public class MainActivity extends AppCompatActivity{
     }
 
     private void getDataFromInternet(){
-        namaMakanan.add("Tahu Gimbal");
-        fotoMakanan.add("https://cdnt.orami.co.id/unsafe/468x324/cdn-cas.orami.co.id/parenting/images/tahu-gimbal.width-800.jpg");
-        infoMakanan.add("Tahu gimbal adalah makanan khas Semarang yang merupakan campuran dari tahu goreng, tempe, gimbal, irisan kol, dan sambal kacang.           Harga : Rp.24.000");
+        namaMakanan.add("orak arik ayam");
+        fotoMakanan.add("https://simomot.com/wp-content/uploads/2017/01/Resep-orak-arik-ayam-wortel-ala-Bunda-Mayrine.jpg");
+        infoMakanan.add("orak arik ayam adalah nasi putih yang disajikan dengan suiran ayam yang masak dengan bumbu yang khas          Harga : Rp.15.000");
 
-        namaMakanan.add("Lumpia Semarang");
-        fotoMakanan.add("https://cdnt.orami.co.id/unsafe/468x351/cdn-cas.orami.co.id/parenting/images/Lumpia_Semarang.width-800.jpg");
-        infoMakanan.add("Berikutnya adalah makanan khas Semarang yang memiliki aroma yang kuat, namanya lumpia. Hal ini dikarenakan jajanan yang satu ini menggunakan rebung sebagai bahan dasarnya.\n" +
-                "\n" +
-                "Selain rebung, lumpia Semarang terdiri dari udang, telur, dan daging. Umumnya, makanan yang digoreng ini dimakan dengan bumbu khas atau acar.             Harga : Rp.21.000");
+        namaMakanan.add("Magelangan");
+        fotoMakanan.add("http://kbu-cdn.com/dk/wp-content/uploads/mie-goreng-magelangan-pedas.jpg");
+        infoMakanan.add("Magelangan adaah nasi goreng yang dimasak dengan campuran mie dan bumbu bumbu rempah pilihan Harga : Rp.21.000");
 
-        namaMakanan.add("Tahu Petis");
-        fotoMakanan.add("https://cdnt.orami.co.id/unsafe/468x264/cdn-cas.orami.co.id/parenting/images/tahu_petis.width-800.jpg");
-        infoMakanan.add("Selain tahu gimbal, ada pula tahu petis yang tak kalah terkenal di Semarang. Makanan khas Semarang ini terkenal nikmat dan khas, karena isinya adalah tahu putih yang diisi dengan petis kemudian digoreng.                                                                                  Harga : Rp.26.000");
+        namaMakanan.add("Es Susu");
+        fotoMakanan.add("https://dapurbahari.com/wp-content/uploads/DSCN1200.jpg");
+        infoMakanan.add("men berikutnya adalah es Susu yang segar                                                                    Harga : Rp.4.000");
 
-        namaMakanan.add("Wingko Babat");
-        fotoMakanan.add("https://cdnt.orami.co.id/unsafe/468x247/cdn-cas.orami.co.id/parenting/images/Wingko_Babat.width-800.jpg");
-        infoMakanan.add("Berikutnya ada wingko babat, makanan khas Semarang rumahan yang terbuat dari beras ketan dan kelapa yang di bakar.                                                                                  Harga : Rp.27.000");
-
-        namaMakanan.add("Pisang Plenet");
-        fotoMakanan.add("https://cdnt.orami.co.id/unsafe/468x311/cdn-cas.orami.co.id/parenting/images/kue-wingko-babat-khas-semarang.width-800.jpg");
-        infoMakanan.add(" Pisang plenet terbuat dari pisang kepok yang dibakar di atas arang, kemudian ditekan dengan papan kecil sehingga bentuknya menjadi gepeng.\n" +
-                "\n                                 Harga : Rp.28.000");
+        namaMakanan.add("Es Teh Manis");
+        fotoMakanan.add("https://s0-bukalapak-com.cdn.ampproject.org/ii/w180/s/s0.bukalapak.com/img/50209595231/s-194-194/data.png");
+        infoMakanan.add("Berikutnya ada Es teh manis yang dapat menyejukkan tenggorokan                                                       Harga : Rp.3.000");
 
         prosesRecyclerViewAdapter();
     }
